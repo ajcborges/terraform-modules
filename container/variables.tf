3d549a42e312c5220aae99b79c99e7327601ba95
Alfredo Cedeno <ajcborges@gmail.com>
2024-03-07 12:34:57 +1100

    Update variables.tf

--- container/variables.tf
+++ container/variables.tf
@@ -1,1 +1,17 @@
+variable "docker_initial_name" {
+  description = "Initial name for the Docker container"
+}
 
+variable "image_name" {
+  description = "Name of the Docker image"
+}
+
+variable "container_port" {
+  description = "Port to expose in the container"
+}
+
+variable "host_port" {
+  description = "Port to expose on the host"
+}
+
+// Add other input variables as needed
