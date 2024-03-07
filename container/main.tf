ec6bda2f5727f760065b25204d6f16974091660a
Alfredo Cedeno <ajcborges@gmail.com>
2024-03-07 12:34:34 +1100

    Update main.tf

--- container/main.tf
+++ container/main.tf
@@ -1,1 +1,11 @@
+resource "docker_container" "my_container" {
+  name  = var.docker_initial_name
+  image = var.image_name
 
+  ports {
+    internal = var.container_port
+    external = var.host_port
+  }
+
+  // Other configurations like environment variables, volumes, etc.
+}
