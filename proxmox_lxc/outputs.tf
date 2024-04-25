d76bd6e23e448b5f0dec071145f64100f3335c88
alfredo cedeno <alfredo@cedeno.me>
2024-04-25 14:47:32 +1000

    ignoring output

--- proxmox_lxc/outputs.tf
+++ proxmox_lxc/outputs.tf
@@ -1,9 +1,9 @@
-output "id" {
-  description = "The Proxmox VMID."
-  value       = proxmox_lxc.lxc.vmid
-}
+# output "id" {
+#   description = "The Proxmox VMID."
+#   value       = proxmox_lxc.lxc.vmid
+# }
 
-output "name" {
-  description = "The lxc host name."
-  value       = proxmox_lxc.lxc.hostname
-}
+# output "name" {
+#   description = "The lxc host name."
+#   value       = proxmox_lxc.lxc.hostname
+# }
