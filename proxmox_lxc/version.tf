9c3cfcb3da7675fd4fdeb26ec2382ca83ffd39ea
alfredo cedeno <alfredo@cedeno.me>
2024-04-29 17:03:43 +1000

    update provider info

--- proxmox_lxc/version.tf
+++ proxmox_lxc/version.tf
@@ -8,14 +8,3 @@ terraform {
   required_version = ">= 1.3.3"
 
 }
-
-provider "proxmox" {
-  pm_api_url = var.proxmox_api_url # Required. This is the target Proxmox API endpoint.
-  #pm_api_token_id     = var.proxmox_api_token_id     # Optional. This is an API token you have previously created for a specific user.
-  #pm_api_token_secret = var.proxmox_api_token_secret # Optional. This uuid is only available when the token was initially created.
-
-  pm_user     = var.proxmox_user     # Optional. Proxmox User (myuser@pam or myuser@pve)
-  pm_password = var.proxmox_password # Optional. Sensitive. Proxmox User password.
-
-  pm_tls_insecure = var.proxmox_tls_insecure # Optional. Disable TLS verification while connecting to the proxmox server.
-}
