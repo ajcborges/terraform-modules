cb4354cbd9995306039d3d323c1cb9325b0903a3
alfredo cedeno <alfredo@cedeno.me>
2024-05-01 15:39:02 +1000

    refactor code.

--- proxmox_lxc/main.tf
+++ proxmox_lxc/main.tf
@@ -1,42 +1,42 @@
 resource "proxmox_lxc" "basic" {
-  target_node = var.proxmox_node_m          # String containing the cluster node name.
-  pool        = var.proxmox_resource_pool_m # The name of the Proxmox resource pool to add this container to.
+  target_node = var.proxmox_node          # String containing the cluster node name.
+  pool        = var.proxmox_resource_pool # The name of the Proxmox resource pool to add this container to.
 
-  ostemplate = var.lxc_ostemplate_m # The volume identifier that points to the OS template or backup file.
-  ostype     = var.lxc_ostype_m     # The operating system type, used by LXC to set up and configure the container. 
-  arch       = var.lxc_arch_m       # Sets the container OS architecture type.
+  ostemplate = var.lxc_ostemplate # The volume identifier that points to the OS template or backup file.
+  ostype     = var.lxc_ostype     # The operating system type, used by LXC to set up and configure the container. 
+  arch       = var.lxc_arch       # Sets the container OS architecture type.
 
-  description = var.lxc_description_m   # Sets the container description seen in the web interface.
-  vmid        = var.lxc_vmid_m          # A number that sets the VMID of the container. If set to 0, the next available VMID is used. Default is 0
-  hostname    = var.lxc_hostname_m      # Specifies the host name of the container.
-  password    = var.lxc_root_password_m # Sets the root password inside the container.
+  description = var.lxc_description   # Sets the container description seen in the web interface.
+  vmid        = var.lxc_vmid          # A number that sets the VMID of the container. If set to 0, the next available VMID is used. Default is 0
+  hostname    = var.lxc_hostname      # Specifies the host name of the container.
+  password    = var.lxc_root_password # Sets the root password inside the container.
   # Multi-line string of SSH public keys that will be added to the container.
   ssh_public_keys = <<-EOF
-  %{for key in var.lxc_public_ssh_keys_m~}
+  %{for key in var.lxc_public_ssh_keys~}
   ${key}
   %{endfor~}
 EOF
 
-  unprivileged = var.lxc_unprivileged_m    # A boolean that makes the container run as an unprivileged user.
-  onboot       = var.lxc_start_on_boot_m   # A boolean that determines if the container will start on boot. 
-  start        = var.lxc_start_on_create_m # A boolean that determines if the container is started after creation.
-  protection   = var.lxc_protection_m      # A boolean that enables the protection flag on this container. 
+  unprivileged = var.lxc_unprivileged    # A boolean that makes the container run as an unprivileged user.
+  onboot       = var.lxc_start_on_boot   # A boolean that determines if the container will start on boot. 
+  start        = var.lxc_start_on_create # A boolean that determines if the container is started after creation.
+  protection   = var.lxc_protection      # A boolean that enables the protection flag on this container. 
 
   features {                                         # An object for allowing the container to access advanced features.
-    fuse    = var.lxc_enable_fuse_m                    # A boolean for enabling FUSE mounts.
-    keyctl  = var.lxc_enable_keyctl_m                  # A boolean for enabling the keyctl() system call.
-    mount   = join(";", var.lxc_allowed_mount_types_m) # Defines the filesystem types (separated by semicolons) that are allowed to be mounted.
-    nesting = var.lxc_enable_nesting_m                 # A boolean to allow nested virtualization.
+    fuse    = var.lxc_enable_fuse                    # A boolean for enabling FUSE mounts.
+    keyctl  = var.lxc_enable_keyctl                  # A boolean for enabling the keyctl() system call.
+    mount   = join(";", var.lxc_allowed_mount_types) # Defines the filesystem types (separated by semicolons) that are allowed to be mounted.
+    nesting = var.lxc_enable_nesting                 # A boolean to allow nested virtualization.
   }
 
-  cores  = var.lxc_cores_m       # The number of cores assigned to the container. A container can use all available cores by default.
-  memory = var.lxc_memory_m      # A number containing the amount of RAM to assign to the container (in MB).
-  swap   = var.lxc_swap_memory_m #A number that sets the amount of swap memory available to the container.
+  cores  = var.lxc_cores       # The number of cores assigned to the container. A container can use all available cores by default.
+  memory = var.lxc_memory      # A number containing the amount of RAM to assign to the container (in MB).
+  swap   = var.lxc_swap_memory #A number that sets the amount of swap memory available to the container.
 
   # Terraform will crash without rootfs defined
   rootfs {                                          # An object for configuring the root mount point of the container. Can only be specified once.
-    storage = var.lxc_rootfs_storage_volume_m         # A string containing the volume , directory, or device to be mounted into the container (at the path specified by mp)
-    size    = format("%sG", var.lxc_rootfs_size_gb_m) # Size of the underlying volume. Must end in T, G, M, or K. Note that this is a read only value.
+    storage = var.lxc_rootfs_storage_volume         # A string containing the volume , directory, or device to be mounted into the container (at the path specified by mp)
+    size    = format("%sG", var.lxc_rootfs_size_gb) # Size of the underlying volume. Must end in T, G, M, or K. Note that this is a read only value.
   }
 
   # mountpoint { # An object for defining a volume to use as a container mount point. Can be specified multiple times.
@@ -51,14 +51,14 @@ EOF
   #   size    = "256G" # Size of the underlying volume. Must end in T, G, M, or K. Note that this is a read only value.
   # }
 
-  nameserver   = var.lxc_dns_name_server_m   # The DNS server IP address used by the container.
-  searchdomain = var.lxc_dns_search_domain_m # Sets the DNS search domains for the container.
+  nameserver   = var.lxc_dns_name_server   # The DNS server IP address used by the container.
+  searchdomain = var.lxc_dns_search_domain # Sets the DNS search domains for the container.
 
   network {                                                   # An object defining a network interface for the container. Can be specified multiple times.
     name     = "eth0"                                         # The name of the network interface as seen from inside the container
     bridge   = "vmbr0"                                        # The bridge to attach the network interface to
-    ip       = join("", ["192.168.30.", var.lxc_vmid_m, "/24"]) # The IPv4 address of the network interface. Can be a static IPv4 address (in CIDR notation).
-    firewall = var.lxc_firewall_m                               # A boolean to enable the firewall on the network interface.
-    gw       = "192.168.10.254"                     # The IPv4 address belonging to the network interface's default gateway.
+    ip       = join("", ["192.168.30.", var.lxc_vmid, "/24"]) # The IPv4 address of the network interface. Can be a static IPv4 address (in CIDR notation).
+    firewall = var.lxc_firewall                               # A boolean to enable the firewall on the network interface.
+    gw       = "192.168.10.254"                               # The IPv4 address belonging to the network interface's default gateway.
   }
 }
--- proxmox_lxc/variables.tf
+++ proxmox_lxc/variables.tf
@@ -1,51 +1,51 @@
 ### main.tf variables
 
-variable "proxmox_node_m" {
+variable "proxmox_node" {
   description = "String containing the cluster node name."
   type        = string
 }
 
-variable "lxc_hostname_m" {
+variable "lxc_hostname" {
   description = "Specifies the host name of the container."
   type        = string
 }
 
-variable "lxc_ostemplate_m" {
+variable "lxc_ostemplate" {
   description = "The volume identifier that points to the OS template or backup file."
   type        = string
   default     = "local:vztmpl/debian-12-standard_12.2-1_amd64.tar.zst"
 }
 
-variable "lxc_root_password_m" {
+variable "lxc_root_password" {
   description = "Sets the root password inside the container."
   type        = string
   default     = null
   sensitive   = true
 
   validation {
-    condition     = var.lxc_root_password_m != ""
+    condition     = var.lxc_root_password != ""
     error_message = "The password must not be empty."
   }
 
   validation {
-    condition     = var.lxc_root_password_m != null ? length(var.lxc_root_password_m) >= 5 : true
+    condition     = var.lxc_root_password != null ? length(var.lxc_root_password) >= 5 : true
     error_message = "The password must be at least 5 characters."
   }
 }
 
-variable "lxc_unprivileged_m" {
+variable "lxc_unprivileged" {
   description = "A boolean that makes the container run as an unprivileged user."
   type        = bool
   default     = false
 }
 
-variable "lxc_description_m" {
+variable "lxc_description" {
   description = "Sets the container description seen in the web interface."
   type        = string
   default     = "Created with Terraform."
 }
 
-variable "lxc_ostype_m" {
+variable "lxc_ostype" {
   description = "The operating system type, used by LXC to set up and configure the container. Automatically determined if not set."
   type        = string
   default     = "debian"
@@ -57,31 +57,31 @@ variable "lxc_public_ssh_keys" {
   default     = []
 }
 
-variable "proxmox_resource_pool_m" {
+variable "proxmox_resource_pool" {
   description = "The name of the Proxmox resource pool to add this container to."
   type        = string
   default     = null
 }
 
-variable "lxc_arch_m" {
+variable "lxc_arch" {
   description = "Sets the container OS architecture type."
   type        = string
   default     = "amd64"
 }
 
-variable "lxc_vmid_m" {
+variable "lxc_vmid" {
   description = "A number that sets the VMID of the container. If set to 0, the next available VMID is used."
   type        = number
   default     = 0
 }
 
-variable "lxc_start_on_boot_m" {
+variable "lxc_start_on_boot" {
   description = "A boolean that determines if the container will start on boot."
   type        = bool
   default     = true
 }
 
-variable "lxc_start_on_create_m" {
+variable "lxc_start_on_create" {
   description = "A boolean that determines if the container is started after creation."
   type        = bool
   default     = false
@@ -93,19 +93,19 @@ variable "lxc_protection" {
   default     = false
 }
 
-variable "lxc_enable_fuse_m" {
+variable "lxc_enable_fuse" {
   description = "A boolean for enabling FUSE mounts."
   type        = bool
   default     = false
 }
 
-variable "lxc_enable_keyctl_m" {
+variable "lxc_enable_keyctl" {
   description = "A boolean for enabling the keyctl() system call."
   type        = bool
   default     = false
 }
 
-variable "lxc_allowed_mount_types_m" {
+variable "lxc_allowed_mount_types" {
   type        = list(string)
   description = "Defines the filesystem types that are allowed to be mounted. Options include 'nfs' and 'smb'."
   default     = []
@@ -123,42 +123,42 @@ variable "lxc_cores" {
   default     = 2
 }
 
-variable "lxc_memory_m" {
+variable "lxc_memory" {
   description = "A number containing the amount of RAM to assign to the container (in MB)."
   type        = number
   default     = 4096
 }
 
-variable "lxc_swap_memory_m" {
+variable "lxc_swap_memory" {
   description = "A number that sets the amount of swap memory available to the container."
   type        = number
   default     = 1024
 }
 
-variable "lxc_rootfs_storage_volume_m" {
+variable "lxc_rootfs_storage_volume" {
   type        = string
   description = "A string containing the volume , directory, or device to be mounted into the container. E.g. local-lvm, local-zfs, local etc."
   default     = "local-lvm"
 }
 
-variable "lxc_rootfs_size_gb_m" {
+variable "lxc_rootfs_size_gb" {
   type        = number
   description = "Size of the underlying volume. Must end in T, G, M, or K. Note that this is a read only value."
   default     = 40
 }
 
-variable "lxc_dns_name_server_m" {
+variable "lxc_dns_name_server" {
   type        = string
   description = "The DNS server IP address used by the container."
   default     = "1.1.1.1"
 }
 
-variable "lxc_dns_search_domain_m" {
+variable "lxc_dns_search_domain" {
   type        = string
   description = "Sets the DNS search domains for the container"
 }
 
-variable "lxc_firewall_m" {
+variable "lxc_firewall" {
   description = "A boolean to enable the firewall on the network interface."
   type        = bool
   default     = false
