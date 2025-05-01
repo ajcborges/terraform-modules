ba73b21102f528395fcf6a6d45fc29654cf01ac7
Alfredo Cedeno <ajcborges@gmail.com>
2025-05-01 16:42:38 +1000

    Update kreuzwerker/docker provider version

--- container/version.tf
+++ container/version.tf
@@ -1,8 +1,8 @@
 terraform {
   required_providers {
     docker = {
-      source = "calxus/docker"
-      version = "3.0.0"
+      source = "kreuzwerker/docker"
+      version = "3.4.0"
     }
   }
   required_version = ">=1.3.3"
