405f801f33791e9c8dee47d42e8d30c09548e56c
Alfredo Cedeno <ajcborges@gmail.com>
2025-05-01 16:34:40 +1000

    update terraform provider version

--- container/version.tf
+++ container/version.tf
@@ -1,8 +1,8 @@
 terraform {
   required_providers {
     docker = {
-      source  = "kreuzwerker/docker"
-      version = ">=3.0"
+      source = "calxus/docker"
+      version = "3.0.0"
     }
   }
   required_version = ">=1.3.3"
